d97e4b3370a650990faacdca37d4e8e5532095af
KingBygone0 <kingbygone@icloud.com>
2023-10-03 05:44:33 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    print(f"{i:02d}", end=", " if i < 99 else "\n")
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print(f"{i:d}")
