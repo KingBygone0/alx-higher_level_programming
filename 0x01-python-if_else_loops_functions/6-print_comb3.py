4e65e31dac928757199b5019d7a3506225f7f619
KingBygone0 <kingbygone@icloud.com>
2023-10-03 03:06:07 -0700

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+for n in range(0, 10):
+    for x in range(0, 10):
+        if n < x:
+            if n == 8:
+                print("{:d}{:d}".format(n, x))
+            else:
+                print("{:d}{:d}".format(n,x), end=", ")
