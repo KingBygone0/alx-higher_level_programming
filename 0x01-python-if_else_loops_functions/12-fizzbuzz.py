b42bbaf0b38df412770ff060ca215af4298fdb50
KingBygone0 <kingbygone@icloud.com>
2023-10-03 03:20:05 -0700

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 != 0 and num % 5 != 0:
+            print(num, end="")
+        if num % 3 == 0:
+            print("Fizz", end="")
+        if num % 5 == 0:
+            print("Buzz", end="")
+        if num != 101:
+            print(" ", end="")
