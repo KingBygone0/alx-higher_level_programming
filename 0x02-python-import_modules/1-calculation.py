b480d9518228bad60a1268dc043345ea76e339ea
KingBygone0 <kingbygone@icloud.com>
2023-10-05 05:39:55 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/python3
+from calculator_1 import add, substract, multiply, divide
+a = 10
+b = 5
+print(f"{a} + {b} = {sum_result}")
+print(f"{a} - {b} = {sub_result}")
+print(f"{a} * {b} = {mul_result}")
+print(f"{a} / {b} = {div_result}")
