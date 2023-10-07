7f9e185215e01d12d5593e544aac2b98dfeedd50
KingBygone0 <kingbygone@icloud.com>
2023-10-06 17:43:45 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        length = 0
+        first = None
+    else:
+        length = len(sentence)
+        first = sentence[0]
+    return length, first
