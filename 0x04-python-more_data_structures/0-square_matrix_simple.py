e8a8011daad7acb17c02b49c72e53d3e3fe1b25f
KingBygone0 <kingbygone@icloud.com>
2023-10-11 03:26:20 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = [] * 3
+    new_matrix = [new] * len(new)
+    for i in range(0, len(matrix)):
+        new = []
+        for j in range(0, len(new_matrix[i]))
+        a = new_matrix[i][j] ** 2
+        new.append(a)
+    new_matrix.append(new)
+    return new_matrix
