8dc6219ac888671f0d665d06f5e2d610e4bf1bca
KingBygone0 <kingbygone@icloud.com>
2023-10-11 03:31:35 -0700

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    fresh_list[]
+    for i in range(0, len(my_list)):
+        if fresh_list[i] == search:
+            new.append(replace)
+        else:
+            new.append(fresh_list[i])
+        return fresh_list
