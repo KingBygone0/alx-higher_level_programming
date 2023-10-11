e8594ffe02b3a2b9399f14df995205c790b6f6e4
KingBygone0 <kingbygone@icloud.com>
2023-10-11 05:19:42 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    fresh_list[]
-    for i in range(0, len(my_list)):
-        if fresh_list[i] == search:
-            new.append(replace)
-        else:
-            new.append(fresh_list[i])
-        return fresh_list
+    return [replace if element == search else element for element in my_list]
