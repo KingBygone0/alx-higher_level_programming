db86d52dfce641a1a2e68d8b80bd7a87426b408c
KingBygone0 <kingbygone@icloud.com>
2023-10-11 06:10:48 -0700

    Complex Delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    return {key: val for key, val in a_dictionary.items() if val != value}
+    for x in a_dictionary.copy():
+        if a_dictionary[x] == value:
+            a_dictionary.pop(x)
+    return a_dictionary
