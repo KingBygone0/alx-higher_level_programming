8ddaf7cf58561deb627da2c039eaef39e3d65086
KingBygone0 <kingbygone@icloud.com>
2023-10-11 05:16:41 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared_matrix = []
-    for row in matrix:
-        squared_row = []
-        for element in row:
-            if isintance(element, int):
-                squared_row.append(element ** 2)
-            else:
-                squared_row.append(element)
-        squared_matrix.append(squared_row)
-    return squared_matrix
+    if not matrix:
+        return None
+    else:
+        return [[element**2 for element in row] for row in matrix]
