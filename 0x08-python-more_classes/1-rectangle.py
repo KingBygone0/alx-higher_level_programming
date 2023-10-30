25567ad643cdb0e1c1cc1117a8c7c3ccf47c20cb
KingBygone0 <kingbygone@icloud.com>
2023-10-30 05:04:33 -0700

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Rectangle class."""
+    def __init__(self, width=0, height=0):
+        """__init__ function. call setter"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width function getter."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width function setter."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height function getter."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height function setter."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
