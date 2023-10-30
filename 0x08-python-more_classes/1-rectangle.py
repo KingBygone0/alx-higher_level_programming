90b4587c1d630070c2887263c375181e3bd53514
KingBygone0 <kingbygone@icloud.com>
2023-10-30 05:05:32 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
+"""create the class"""
+
+
 class Rectangle:
-    """Rectangle class."""
+    """initialize"""
     def __init__(self, width=0, height=0):
-        """__init__ function. call setter"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """width function getter."""
+        """the getter of width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """width function setter."""
-        if type(value) is not int:
+        """the setter in width"""
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -22,13 +24,13 @@ class Rectangle:
 
     @property
     def height(self):
-        """height function getter."""
+        """the setter of self in height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """height function setter."""
-        if type(value) is not int:
+        """the getter of self in height"""
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
