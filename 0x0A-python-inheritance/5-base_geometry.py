b4cf5a0dac2b780a0a5833dd09fefafcbc00461b
KingBygone0 <kingbygone@icloud.com>
2023-11-06 07:45:23 -0800

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""BaseGeometry - empty class
+
+   Description: return empty class
+   Return: empty class
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+    pass
