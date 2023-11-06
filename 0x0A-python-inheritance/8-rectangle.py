c11db8cc6fffb5c9245700bb733c0a62ba05a165
KingBygone0 <kingbygone@icloud.com>
2023-11-06 07:49:47 -0800

    8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python3
-"""BaseGeometry - module basegeometry class
-
-   Description: class basegeometry
-   Return: basegeometry class
-"""
-
-
-class BaseGeometry:
-    """BaseGeometry class."""
-
-    def area(self):
-        """area function."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """integer_validator function."""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name, value))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name, value))
+"""Inheris from baseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class."""
+    """a class to define rectangle using BaseGeometry"""
 
     def __init__(self, width, height):
-        """init function."""
-        Rectangle.integer_validator(self, "width", width)
-        Rectangle.integer_validator(self, "height", height)
+        """Intialize a new Rectangle
+        """
+        self.integer_validator("width", width)
         self.__width = width
-        self.__height = height
+        self.integer_validator("height", height)
+        self.__height = heightt
