aa59aabe7833654733ab27326be55abf7b56efb2
KingBygone0 <kingbygone@icloud.com>
2023-11-07 09:15:41 -0800

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-""" My class module
-"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """ Student class
-    """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
+        """Initializes a new Student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """to_json function"""
-        return self
-
     def to_json(self, attrs=None):
-        """to_json with parameters"""
-        new_dict = {}
-        if attrs is None:
-            return self.__dict__
-        else:
-            for i in attrs:
-                if i == "first_name":
-                    new_dict.update({'first_name': self.first_name})
-                if i == "last_name":
-                    new_dict.update({'last_name': self.last_name})
-                if i == "age":
-                    new_dict.update({'age': self.age})
-            return new_dict
+        """Get a dictionary representation of the Student
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
