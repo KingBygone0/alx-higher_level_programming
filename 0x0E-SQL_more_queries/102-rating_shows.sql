a7bafe3fb900e54684bdab29e883236385e78ca9
KingBygone0 <KingBygone@icloud.com>
2023-12-13 08:06:09 -0800

    102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,11 @@
--- point 19
--- lists all shows from hbtn_0d_tvshows_rate Each record should display: tv_shows.title - rating sum Results must be sorted in descending order by the rating
-SELECT tv_shows.title, COUNT(tv_show_ratings.rate) AS rating
-FROM tv_show_ratings
-RIGHT JOIN tv_shows
-ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY tv_shows.title, tv_show_ratings.show_id
+-- lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in acending order by the rating
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
 ORDER BY rating DESC;
